5935b9f21aaae68c0953786e929b27e91d179aad
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-04-27 23:24:46 +0200

    added the changes to the script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello from Nerdbord!')
+setInterval(() => {
+  const box = document.querySelector(".box");
+  const colors = ["red", "blue", "green", "yellow"];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  box.style.borderColor = randomColor;
+}, 3000);
